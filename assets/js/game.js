381d7ca19e29b048d2bc0a2eb45302b4f4e6a852
thomps0189 <angelathompson2011@gmail.com>
2021-10-19 16:03:26 -0500

    js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an Alert! Javascript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robot's name is " + playerName);
+
+//fight();
